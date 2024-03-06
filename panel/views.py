f5465a5845d13f94dee43d748c9170034c80a854
Eugene Checkoy <mfalcon@mail.ru>
2024-03-06 13:44:55 +0300

    1

--- panel/views.py
+++ panel/views.py
@@ -5,7 +5,7 @@ from panel.models import Header
 
 
 def get_events():
-    url = 'http://obs-balashiha.ru/api/v1/event_list/'  # Замените на URL вашего API
+    url = 'https://obs-balashiha.ru/api/v1/event_list/'  # Замените на URL вашего API
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -14,7 +14,27 @@ def get_events():
 
         for item in data:
             if (
-                item.get('library') == 'Детская библиотека (ш. Энтузиастов 33)' and
+                item.get('library') == 'Информационно-культурный центр' and
+                item.get('date') >= str(date.today())
+            ):
+                filtered_data.append(item)
+
+        return filtered_data
+    else:
+        # Обработка ошибки запроса к API
+        return []
+
+
+def get_movies():
+    url = 'https://obs-balashiha.ru/api/v1/cinema-week/'  # Замените на URL вашего API
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        filtered_data = []
+
+        for item in data:
+            if (
                 item.get('date') >= str(date.today())
             ):
                 filtered_data.append(item)
@@ -48,13 +68,15 @@ def get_visible_header():
 
 def index(request):
     events_data = get_events()
+    movies_data = get_movies()
     header = get_visible_header()
-    return render(request, 'index.html', {'events_data': events_data, 'header': header})
+    return render(request, 'index.html', {'events_data': events_data, 'movies_data': movies_data,
+                                          'header': header})
 
 # API SECTION
 
 def get_news():
-    url = 'http://obs-balashiha.ru/api/v1/news_list/'  # Замените на URL вашего API
+    url = 'https://obs-balashiha.ru/api/v1/news_list/'  # Замените на URL вашего API
     response = requests.get(url)
 
     if response.status_code == 200:
