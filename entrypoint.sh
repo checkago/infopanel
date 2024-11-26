92b2f7d6d77ebdc447cf28477ff8ed949cb79444
Eugene Checkoy <mfalcon@mail.ru>
2024-11-26 11:13:15 +0300

    сайт Балашихи

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Выполнение миграций и других необходимых команд
-python manage.py migrate --run-syncdb
-python manage.py loaddata dbdump.json
+#python manage.py migrate --run-syncdb
+#python manage.py loaddata dbdump.json
 #python manage.py collectstatic --no-input
 gunicorn infopanel.wsgi:application --bind 0.0.0.0:8880 --reload -w 4
